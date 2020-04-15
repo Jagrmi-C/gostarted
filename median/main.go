de7d153cfd63dbd23c7c309efa954b058e93b790
Andrey Romanchik <andrey.romanchik@solbeg.com>
2020-04-15 10:33:47 +0300

    #lection2 - FIX: After review(1).

--- median/main.go
+++ median/main.go
@@ -8,25 +8,28 @@ import (
 
 func median(intsArr []int) float64 {
 	var answer float64
+	sort.Ints(intsArr)
+	log.Println("Sorted list:", intsArr)
 	val := len(intsArr) / 2
 	log.Println("Half value: ", val)
 	if len(intsArr)%2 == 1 {
 		log.Println("%2 == 1")
 		answer = float64(intsArr[val])
 		log.Println("Answer:", answer)
-	} else {
-		log.Println("%2 == 0")
-		answer = (float64(intsArr[val-1]) + float64(intsArr[val])) / 2
-		log.Println("Answer:", answer)
+		return answer
 	}
+
+	log.Println("%2 == 0")
+	answer = (float64(intsArr[val-1]) + float64(intsArr[val])) / 2
+	log.Println("Answer:", answer)
+
 	return answer
 }
 
 func main() {
-	intsArr := []int{7, 2, 4, 8, 1, 2, 9}
-	// intsArr := []int{7, 2, 4, 8, 1, 9}
-	sort.Ints(intsArr)
-	log.Println("Sorted list:", intsArr)
-	medianVal := median(intsArr)
-	fmt.Println(medianVal)
+	intsArrOdd := []int{7, 2, 4, 8, 1, 2, 9}
+	intsArrEven := []int{7, 2, 4, 8, 1, 9}
+	medianVal1 := median(intsArrOdd)
+	medianVal2 := median(intsArrEven)
+	fmt.Println(medianVal1, medianVal2)
 }
--- square/main.go
+++ square/main.go
@@ -2,33 +2,33 @@ package main
 
 import "fmt"
 
-// Point is position in space
+// Point type represents a point in Cartesian coordinate system
 type Point struct {
 	x, y int
 }
 
-// Square is equilateral rectangle
+// Square type is equilateral rectangle
 type Square struct {
-	start Point
-	a     uint
+	start Point // start position in Cartesian coordinate system
+	a     uint  // side of a square
 }
 
-// Perimeter - calculate perimetr for Square
+// Perimeter method - calculate perimetr for Square
 func (s Square) Perimeter() int {
 	return int(s.a) * 4
 }
 
-// Area - calculate area for Square
+// Area method - calculate area for Square
 func (s Square) Area() int {
 	return int(s.a) * int(s.a)
 }
 
-// End - Point after made ...
+// End method - return Point type end position in Cartesian coordinate system
 func (s Square) End() Point {
-	newX := s.start.x + int(s.a)
-	newY := s.start.y - int(s.a)
-	endPoint := Point{newX, newY}
-	return endPoint
+	return Point{
+		x: s.start.x + int(s.a),
+		y: s.start.y - int(s.a),
+	}
 }
 
 func main() {
